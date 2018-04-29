e421c355ba39b87c8cc5416c4878ea341ba7990c
DiKang Li <121960425@qq.com>
2018-04-29 13:03:32 +0800

    2014/04/29 更新

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const config = {
   // "base_url": "https://mbook.lilyplay.com",
-  "base_url": "http://localhost:5000"
+  "base_url": "https://mbook.andylistudio.com",
+  // "base_url": "http://localhost:5000"
 }
 module.exports = config
